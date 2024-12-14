8983e519ea7ac093cd19febc8bbc14d4b9a11c85
ludrol <37674089+ludrol@users.noreply.github.com>
2024-12-14 10:39:16 +0100

    added basic generator interaction under E

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -1,9 +1,14 @@
 extends Node2D
 @export var speed = 400
+var area
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	area = get_node("Area2D")
+	
+	#pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,3 +30,24 @@ func _process(delta):
 		#$AnimatedSprite2D.stop()
 		
 		position += velocity * delta
+		
+	
+	if Input.is_action_just_pressed("interact"):
+		#print("generator interaction!")
+		
+		
+		#if self.get_node(Area2D)
+		
+		
+		
+		#
+		for body in area.get_overlapping_areas():
+			print(body)
+			if body.is_in_group("generators group"):  
+				print("generator interaction!")
+				#
+		
+	
+		
+		
+		
