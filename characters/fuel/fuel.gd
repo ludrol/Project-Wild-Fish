f729d677f6671cb21b72fa811b2d88c661c0889c
ludrol <37674089+ludrol@users.noreply.github.com>
2024-12-15 13:37:30 +0100

    added fuel interaction

--- characters/fuel/fuel.gd
+++ characters/fuel/fuel.gd
@@ -3,18 +3,30 @@ extends Node2D
 signal fuel_pickup
 @export var fuelValue = 100
 var area
-
+var player
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	player = get_node_or_null("../Player") #this is really hacky solution. it will break. 
 	area = get_node("Area2D")
+	if player == null:
+		print("you dumbass check " + str(self))
+	player.fuel_interaction.connect(_on_player_fuel_interaction)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
 	
+	
+
+func _on_player_fuel_interaction():
 	for body in area.get_overlapping_areas():
-			print(body)
-			if body.is_in_group("players group"):  
-				print("players interaction!")
-				emit_signal("fuel_pickup")
+		if body.is_in_group("players group"):  
+			GlobalVariables.currentFuel += fuelValue
+			print("current fuel: " + str(GlobalVariables.currentFuel))
+			#for child in self.get_children():
+				#child.queue_free()
+			queue_free()
+				
+				
