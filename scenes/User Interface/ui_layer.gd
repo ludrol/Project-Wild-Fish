3ee61a9083e9ce6cfc4797ba2048e7d96662958e
ludrol <37674089+ludrol@users.noreply.github.com>
2024-12-16 23:26:47 +0100

    UI revamp and setup proper viewport scalinng
    UI revamp and setup proper viewport scalinng nad stuff

--- scenes/User Interface/ui_layer.gd
+++ scenes/User Interface/ui_layer.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 
 @onready var fuelBar = %FuelBar
-
+@onready var Temper = %Temperature
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,7 +11,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
-	
-	
+	fuelBar.value = float(GlobalVariables.playerFuel) / GlobalVariables.maxPlayerFuel * 100
+	Temper.value = float(GlobalVariables.playerFreeze) / GlobalVariables.maxPlayerFreeze * 100
+	#print(float(GlobalVariables.playerFuel) / GlobalVariables.maxPlayerFuel)
 	
 	
--- scenes/global/global_variables.gd
+++ scenes/global/global_variables.gd
@@ -2,12 +2,12 @@ extends Node
 
 @export_category("starting values")
 @export var playerFuel = 100
-@export var playerFreeze = 100
+@export var playerFreeze = 1000
 @export var monsterFreeze = 100
 @export var generatorFuel = 100
 
 @export_category("max values")
-@export var maxPlayerFuel = 1000
+@export var maxPlayerFuel = 500
 @export var maxPlayerFreeze = 1000
 @export var maxMonsterFreeze = 1000
 @export var maxGeneratorFuel = 1000
